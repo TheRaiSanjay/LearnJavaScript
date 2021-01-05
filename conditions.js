e4c7b52982df0b35a06b89f1f7ad7cf3cd40810b
TheRaiSanjay <58761836+TheRaiSanjay@users.noreply.github.com>
2021-01-05 18:42:15 +0530

    Create conditions.js

---
+++ conditions.js
@@ -0,0 +1,16 @@
+var cityName = prompt("Enter your City Name");
+switch (cityName) {
+    case "Kolkata":
+        document.write("Welcome to city of Joy!");
+        break;
+    case "Bhopal":
+        document.write("Welcome to City of Lakes!")
+        break;
+    case "Patna":
+        document.write("Welcome to City of Holy River Ganges!");
+        break;
+    default:
+        document.write("Seems you have lost!");
+        break;
+
+}
