9924b700c9e8954bb6b9e29a4a5c0bb27a3e88ae
TheRaiSanjay <58761836+TheRaiSanjay@users.noreply.github.com>
2021-01-05 18:43:15 +0530

    Update conditions.js
    updating statements.

--- conditions.js
+++ conditions.js
@@ -1,13 +1,13 @@
 var cityName = prompt("Enter your City Name");
 switch (cityName) {
     case "Kolkata":
-        document.write("Welcome to city of Joy!");
+        document.write("Welcome to the city of Joy!");
         break;
     case "Bhopal":
-        document.write("Welcome to City of Lakes!")
+        document.write("Welcome to the City of Lakes!")
         break;
     case "Patna":
-        document.write("Welcome to City of Holy River Ganges!");
+        document.write("Welcome to the City of Holy River Ganges!");
         break;
     default:
         document.write("Seems you have lost!");
