e87e8fc1bce7a829c8fec0bae4c5e7faab6d7dc0
TheRaiSanjay <58761836+TheRaiSanjay@users.noreply.github.com>
2021-01-06 12:21:29 +0530

    Create operators.js

---
+++ operators.js
@@ -0,0 +1,27 @@
+//Arithmetic Operators
+var add=10+11;
+console.log("addition- "+add);
+var diff=10-3;
+console.log("difference- "+diff);
+var product=9*8;
+console.log("product- "+product);
+var div=6/2;
+console.log("div- "+div);
+var mod=5%3;
+console.log("mod- "+mod);
+//Increment and Decrement Operators
+product++
+console.log("product++ :"+product);
+diff--;
+console.log("diff-- :"+diff);
+//Comparision Operators
+var s1=10;
+var s2="10";
+if(s1==s2)
+{console.log("If Part");}
+else
+{console.log("Else Part");}
+if(s1===s2)
+{ console.log("If Part");}
+else
+{console.log("Else Part ===");}
